a25a9e2f6257e3a18638618e1d061d1241eeff31
Justman123 <96775059+Justman123@users.noreply.github.com>
2025-01-08 17:03:01 +0900

    T

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def read_root(query: str):
 
 # URL 리스트
 urls = [
-    "https://drive.google.com/uc?id=1vdQ6-eoetgIxNDh8NTAVm9S_Zu3RwBds",
-    "https://drive.google.com/uc?id=1Ys10nBs-gvAEetmyjTXLlKp8YZpzCH2H",
-    "https://drive.google.com/uc?id=1sGINbXMp3bKmu_5OeGkz8yLpF8m8R4o2",
+    # "https://drive.google.com/uc?id=1vdQ6-eoetgIxNDh8NTAVm9S_Zu3RwBds",
+    # "https://drive.google.com/uc?id=1Ys10nBs-gvAEetmyjTXLlKp8YZpzCH2H",
+    # "https://drive.google.com/uc?id=1sGINbXMp3bKmu_5OeGkz8yLpF8m8R4o2",
     "https://drive.google.com/uc?id=1RJo8w01-dZyRDAxKDBCCEzWo64kZupOl",
 
 ]
