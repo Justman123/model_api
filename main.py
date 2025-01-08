a8f86157ce595624016dab0b31dd81573bd2f636
Justman123 <96775059+Justman123@users.noreply.github.com>
2025-01-08 20:16:54 +0900

    ADD

--- main.py
+++ main.py
@@ -28,17 +28,15 @@ def read_root(query: str):
 # google drive에서 model.pt 다운로드
 
 # URL 리스트
-urls = [
-    "https://drive.google.com/uc?id=1HneFlLGphn4whQQ7Zq9NDJLcGpOaPZCc",
-    "https://drive.google.com/uc?id=1s2vnnVGr6Wq3QGFMf_t1Kgordj4d4LX0",
-    # "https://drive.google.com/uc?id=1V_3hQ0Dl7eLqPqBFV9Y1RVFaCnHpGc6Q",
-    # "https://drive.google.com/uc?id=1wECnYHg9XOzYEYx-ab6SvuZy9pj4pRcZ",
+urls1 = [
+    "https://drive.google.com/uc?id=115TKxODj7F8oUyxwSBIJCF2md90cc2e8",
+    "https://drive.google.com/uc?id=1dRPT9QBzBMuKUAEOx6lVqRI-vbylwDnL",
 ]
 # 외부에서 선언된 i (다운로드 시작 인덱스)
 i = 2  # 시작 인덱스를 설정
 
 # model_pt_chunk 파일 다운로드
-for url in urls:
+for url in urls1:
     output_file = f"model_pt_folder/chunk_{i}.bin"  # 기본 파일 이름 설정
     print(f"Downloading {url} to {output_file}...")
     try:
@@ -47,9 +45,26 @@ for url in urls:
         print(f"Failed to download {url}: {e}")
     i += 1  # 다음 인덱스로 증가
 
+print('1차 다운 완료!')
 # 압축 파일 병합
 parts2 = ['bertmodel_folder/chunk_0.bin', 'bertmodel_folder/chunk_1.bin', 'bertmodel_folder/chunk_2.bin', 'bertmodel_folder/chunk_3.bin']
 merge_files('kobert_base_v1/model.safetensors', parts2)
+
+# model_pt_chunk 파일 다운로드
+urls2 = [
+    "https://drive.google.com/uc?id=19sWrbp_oUFETVATh2T3qziSOZKyT7D64",
+    "https://drive.google.com/uc?id=1pBYSD4VO7hsL0sMzakH7_FCHlaK-brL7",
+] 
+
+for url in urls2:
+    output_file = f"model_pt_folder/chunk_{i}.bin"  # 기본 파일 이름 설정
+    print(f"Downloading {url} to {output_file}...")
+    try:
+        gdown.download(url, output_file, quiet=False)
+    except Exception as e:
+        print(f"Failed to download {url}: {e}")
+    i += 1  # 다음 인덱스로 증가
+print('2차 다운 완료!')
 
 parts1 = ['model_pt_folder/chunk_0.bin', 'model_pt_folder/chunk_1.bin', 'model_pt_folder/chunk_2.bin', 'model_pt_folder/chunk_3.bin', 'model_pt_folder/chunk_4.bin', 'model_pt_folder/chunk_5.bin']
 merge_files('model_state_dict.pt', parts1)
