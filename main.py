39060f5ced4c9167a925a3b29c1d433a55080854
Justman123 <96775059+Justman123@users.noreply.github.com>
2025-01-08 16:50:57 +0900

    Gdown

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch
 from transformers import BertModel
 from kobert_tokenizer import KoBERTTokenizer
 import gluonnlp as nlp
+import gdown
 
 # 압축 파일 병합 함수
 def merge_files(output_path, input_parts):
@@ -24,6 +25,30 @@ def read_root():
 def read_root(query: str):
     return {"query" : query}
 
+# google drive에서 model.pt 다운로드
+
+# URL 리스트
+urls = [
+    "https://drive.google.com/uc?id=1vdQ6-eoetgIxNDh8NTAVm9S_Zu3RwBds",
+    "https://drive.google.com/uc?id=1Ys10nBs-gvAEetmyjTXLlKp8YZpzCH2H",
+    "https://drive.google.com/uc?id=1sGINbXMp3bKmu_5OeGkz8yLpF8m8R4o2",
+    "https://drive.google.com/uc?id=1RJo8w01-dZyRDAxKDBCCEzWo64kZupOl",
+
+]
+
+# 외부에서 선언된 i (다운로드 시작 인덱스)
+i = 1  # 시작 인덱스를 설정
+
+# 파일 다운로드
+for url in urls:
+    output_file = f"model_pt_folder/chunk_{i}"  # 기본 파일 이름 설정
+    print(f"Downloading {url} to {output_file}...")
+    try:
+        gdown.download(url, output_file, quiet=False)
+    except Exception as e:
+        print(f"Failed to download {url}: {e}")
+    i += 1  # 다음 인덱스로 증가
+
 # 압축 파일 병합
 parts1 = ['model_pt_folder/chunk_0.bin', 'model_pt_folder/chunk_1.bin', 'model_pt_folder/chunk_2.bin', 'model_pt_folder/chunk_3.bin']
 merge_files('model_state_dict.pt', parts1)
@@ -36,7 +61,7 @@ print('bertmodel 다운 중...')
 bertmodel = BertModel.from_pretrained("kobert_base_v1", return_dict=False)
 print('bertmodel 다운 완료!')
 
-print('tokenizer 다운 중...')
+# print('tokenizer 다운 중...')
 tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
 vocab = nlp.vocab.BERTVocab.from_sentencepiece(tokenizer.vocab_file, padding_token='[PAD]')
 tok = tokenizer.tokenize
