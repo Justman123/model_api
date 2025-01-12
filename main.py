d78a1abfbabe4ae09690aa661d1ce47cafd20888
Justman123 <96775059+Justman123@users.noreply.github.com>
2025-01-12 11:52:05 +0900

    END

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ def read_root(query: str):
 total, used, free = shutil.disk_usage('/')
 print(total, used, free)
 
-# parts1 = ['chunk_0.bin']
-# merge_files('model_state_dict.pt', parts1)
+parts1 = ['chunk_0.bin', 'chunk_1.bin', 'chunk_2.bin', 'chunk_3.bin']
+merge_files('model_state_dict.pt', parts1)
 
 parts2 = ['bertmodel_folder/chunk_0.bin', 'bertmodel_folder/chunk_1.bin', 'bertmodel_folder/chunk_2.bin', 'bertmodel_folder/chunk_3.bin']
 merge_files('kobert_base_v1/model.safetensors', parts2)
@@ -53,11 +53,6 @@ tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
 vocab = nlp.vocab.BERTVocab.from_sentencepiece(tokenizer.vocab_file, padding_token='[PAD]')
 tok = tokenizer.tokenize
 print('tokenizer 다운 완료!')
-
-# model_state_dict.pt 다운로드
-print("model_state_dict 다운 중..")
-
-print("model_state_dict 다운 완료..")
 
 
 # Bert DataSet & Classifier
