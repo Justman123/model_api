3fee9423f90aa112d249cc389f0e43037080ab68
Justman123 <96775059+Justman123@users.noreply.github.com>
2025-01-13 09:44:24 +0900

    Fix CORS Error

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 def predict(query):
     if (query == "정말 기분 좋다"):
         return '기쁨'
     elif (query == "너무 짜증나"):
         return '화남'
-# API Routing
+    return ""
+
 app = FastAPI()
+
+# CORS middleware
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# API Routing
 @app.get("/")
 def read_root():
     return {"Let's Start!"}
@@ -14,3 +31,4 @@ def read_root():
 @app.get("/model")
 def read_root(query: str):
     return {"sentiment" : predict(query)}
+
